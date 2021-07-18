8167bdf707561717ea5194270635e042226c0cc3
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-07-18 13:53:08 -0500

    made classes

--- src/template.js
+++ src/template.js
@@ -1,5 +1,4 @@
 
-
 buildTemplate = () => {
     template = `
     <!doctype html>
@@ -11,7 +10,7 @@ buildTemplate = () => {
     <title>Team Profile</title>
   </head>
   <body>
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
 </html>
