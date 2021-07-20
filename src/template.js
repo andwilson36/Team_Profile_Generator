848f10e0cbb66858dd9b45b99dafcbac15b995b6
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-07-19 21:12:11 -0500

    functional, needs readme and refactor

--- src/template.js
+++ src/template.js
@@ -10,10 +10,11 @@ generateWebsite.prototype.build = function (teamArray) {
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <link rel="stylesheet" type="text/css" href="./style.css">
       <title>Team Profile</title>
     </head>
     <body>
-      <header class="text-center pt-4 pb-4 bg-success">
+      <header class="text-center pt-4 pb-4 bg-success mb-3">
           <h1 class="text-light">Team Profile</h1>
       </header>
       <div class="container">
@@ -30,7 +31,7 @@ generateWebsite.prototype.build = function (teamArray) {
 `;
 
   function writeToFile(teamArray) {
-    fs.writeFile(`index.html`, buildTemplate(teamArray), (err) =>
+    fs.writeFile(`./dist/index.html`, buildTemplate(teamArray), (err) =>
       err ? console.error(err) : console.log('Profile has been made.')
     );
   }
@@ -39,7 +40,7 @@ generateWebsite.prototype.build = function (teamArray) {
     for (let i = 0; i < teamArray.length; i++) {
       if (teamArray[i].role === 'Manager') {
         card = `
-          <div class="card m-3">
+          <div class="card m-3 w-10">
             <div class="card-header bg-success text-light">
               <h2 class="card-title">${teamArray[i].emName}</h2>
               <h3 class="card-title">Manager</h3>
@@ -47,18 +48,17 @@ generateWebsite.prototype.build = function (teamArray) {
             <div class="card-body">
               <ul class="list-group">
                 <li class="list-group-item">ID: ${teamArray[i].id}</li>
-                <li class="list-group-item">Email: ${teamArray[i].email}</li>
+                <li class="list-group-item">Email: <a href="mailto:${teamArray[i].email}" target="_blank">${teamArray[i].email}</a></li>
                 <li class="list-group-item">Office Number: ${teamArray[i].officeNumber}</li>
               </ul>
             </div>
           </div>
           `
         teamArray.push(card);
-        teamArray.shift();
       }
       if (teamArray[i].role === 'Engineer') {
         card = `
-          <div class="card m-3">
+          <div class="card m-3 w-10">
             <div class="card-header bg-success text-light">
               <h2 class="card-title">${teamArray[i].emName}</h2>
               <h3 class="card-title">Engineer</h3>
@@ -66,18 +66,17 @@ generateWebsite.prototype.build = function (teamArray) {
             <div class="card-body">
               <ul class="list-group">
                 <li class="list-group-item">ID: ${teamArray[i].id}</li>
-                <li class="list-group-item">Email: ${teamArray[i].email}</li>
-                <li class="list-group-item">GitHub: ${teamArray[i].gitub}</li>
+                <li class="list-group-item">Email: <a href="mailto:${teamArray[i].email}" target="_blank">${teamArray[i].email}</a></li>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${teamArray[i].github}" target="_blank">${teamArray[i].github}</a></li>
               </ul>
             </div>
           </div>
         `
-        teamArray.push(card)
-        teamArray.shift();
+        teamArray.push(card);
       }
       if (teamArray[i].role === 'Intern') {
         card = `
-          <div class="card m-3">
+          <div class="card m-3 w-10">
             <div class="card-header bg-success text-light">
               <h2 class="card-title">${teamArray[i].emName}</h2>
               <h3 class="card-title">Intern</h3>
@@ -85,18 +84,19 @@ generateWebsite.prototype.build = function (teamArray) {
             <div class="card-body">
               <ul class="list-group">
                 <li class="list-group-item">ID: ${teamArray[i].id}</li>
-                <li class="list-group-item">Email: ${teamArray[i].email}</li>
+                <li class="list-group-item">Email: <a href="mailto:${teamArray[i].email} target="_blank"">${teamArray[i].email}</a></li>
                 <li class="list-group-item">School: ${teamArray[i].school}</li>
               </ul>
             </div>
           </div>
         `
-        teamArray.push(card)
-        teamArray.shift();
+        teamArray.push(card);
       }
     }
-    console.log(teamArray);
-    console.log(teamArray);
+    numOfObjects = teamArray.length / 2;
+    for(let i = 0; i < numOfObjects; i++) {
+      teamArray.shift();
+    }
     return teamArray.join(' ');
   }
 
