66fcecf29da8585f151c76a8b1d6af4126f4b0cb
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-07-19 20:35:50 -0500

    works, needs styling

--- src/template.js
+++ src/template.js
@@ -1,80 +1,109 @@
 const fs = require('fs');
 
-cardArray = [];
-
 function generateWebsite() { }
 
 generateWebsite.prototype.build = function (teamArray) {
+  let templateStart = `
+  <!doctype html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <title>Team Profile</title>
+    </head>
+    <body>
+      <header class="text-center pt-4 pb-4 bg-success">
+          <h1 class="text-light">Team Profile</h1>
+      </header>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 d-flex justify-content-center">
+      `;
+  let templateEnd = `
+          </div>
+        </div>
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </body>
+  </html>
+`;
 
   function writeToFile(teamArray) {
     fs.writeFile(`index.html`, buildTemplate(teamArray), (err) =>
       err ? console.error(err) : console.log('Profile has been made.')
     );
   }
-
-  buildTemplate = (teamArray) => {
-    template = `
-      <!doctype html>
-      <html lang="en">
-        <head>
-          <meta charset="utf-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1">
-          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-          <title>Team Profile</title>
-        </head>
-        <body>
-          <header class="text-center pt-4 pb-4 bg-success">
-              <h1 class="text-light">Team Profile</h1>
-          </header>
-          ` + makeCards(teamArray) + `
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        </body>
-      </html>
-      `
-  }
-
-  makeCards = (array) => {
-    for (let i = 0; i < array.length; i++) {
-      if (array[i].role === 'Manager') {
+ 
+  makeCards = (teamArray) => {
+    for (let i = 0; i < teamArray.length; i++) {
+      if (teamArray[i].role === 'Manager') {
         card = `
-          <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-            <div class="card-header">${array[i].role}</div>
+          <div class="card m-3">
+            <div class="card-header bg-success text-light">
+              <h2 class="card-title">${teamArray[i].emName}</h2>
+              <h3 class="card-title">Manager</h3>
+            </div>
             <div class="card-body">
-              <h5 class="card-title">${array[i].emName}</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <ul class="list-group">
+                <li class="list-group-item">ID: ${teamArray[i].id}</li>
+                <li class="list-group-item">Email: ${teamArray[i].email}</li>
+                <li class="list-group-item">Office Number: ${teamArray[i].officeNumber}</li>
+              </ul>
             </div>
           </div>
           `
-        cardArray.push(card);
+        teamArray.push(card);
+        teamArray.shift();
       }
-      if (array[i].role === 'Engineer') {
+      if (teamArray[i].role === 'Engineer') {
         card = `
-        <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-          <div class="card-header">${array[i].role}</div>
-          <div class="card-body">
-            <h5 class="card-title">${array[i].emName}</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div class="card m-3">
+            <div class="card-header bg-success text-light">
+              <h2 class="card-title">${teamArray[i].emName}</h2>
+              <h3 class="card-title">Engineer</h3>
+            </div>
+            <div class="card-body">
+              <ul class="list-group">
+                <li class="list-group-item">ID: ${teamArray[i].id}</li>
+                <li class="list-group-item">Email: ${teamArray[i].email}</li>
+                <li class="list-group-item">GitHub: ${teamArray[i].gitub}</li>
+              </ul>
+            </div>
           </div>
-        </div>
         `
-        cardArray.push(card);
+        teamArray.push(card)
+        teamArray.shift();
       }
-      if (array[i].role === 'Intern') {
+      if (teamArray[i].role === 'Intern') {
         card = `
-        <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-          <div class="card-header">${array[i].role}</div>
-          <div class="card-body">
-            <h5 class="card-title">${array[i].emName}</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div class="card m-3">
+            <div class="card-header bg-success text-light">
+              <h2 class="card-title">${teamArray[i].emName}</h2>
+              <h3 class="card-title">Intern</h3>
+            </div>
+            <div class="card-body">
+              <ul class="list-group">
+                <li class="list-group-item">ID: ${teamArray[i].id}</li>
+                <li class="list-group-item">Email: ${teamArray[i].email}</li>
+                <li class="list-group-item">School: ${teamArray[i].school}</li>
+              </ul>
+            </div>
           </div>
-        </div>
         `
-        cardArray.push(card);
+        teamArray.push(card)
+        teamArray.shift();
       }
-      return cardArray.join('+');
     }
+    console.log(teamArray);
+    console.log(teamArray);
+    return teamArray.join(' ');
+  }
 
-
+  function buildTemplate(teamArray) {
+      let template  = templateStart + makeCards(teamArray) + templateEnd;
+      console.log(template);
+      return template; 
   }
   writeToFile(teamArray);
 }
